38ad9b72abd19055c6bbc0d95e122279dce3582a
Breno <figueire@ualberta.ca>
2022-04-20 07:40:54 -0600

    Start low rank reconstruction

---
+++ src/denoise/LANCSSAOp.jl
@@ -0,0 +1,14 @@
+function LANCSSAOp(IN,k)
+
+    # Hankelize
+    H = HankelOp(IN);
+    
+    # Rank reduction
+    U, Bk, V = HCDSP.lanbpro(H,k)
+    
+    # Averaging
+    OUT = AveragingOp(U*U'*H,size(IN))
+
+    return OUT
+end
+
